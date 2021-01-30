83251bf93823c7700c5cf3b2f745d4f02b12c717
pi4 <saikat626@gmail.com>
2021-01-30 03:00:06 +0000

    Update wrapper script so that cronjob can run the script from anywhere

--- monitor_ip.sh
+++ monitor_ip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Script wraps over the main python script. Crontab runs this script
+cd $(dirname $0)
 source venv/bin/activate
 ./detect_ip_change.py >> tmp/detect_ip.log
 deactivate
+cd -
